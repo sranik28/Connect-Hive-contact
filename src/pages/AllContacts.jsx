64e65d9831b402621000047addc521761ea137ec
MD Anik <122194751+sranik28@users.noreply.github.com>
2024-01-03 20:38:24 +0600

    json added

--- src/pages/AllContacts.jsx
+++ src/pages/AllContacts.jsx
@@ -2,9 +2,68 @@ import React from "react";
 
 const AllContacts = () => {
   return (
-    <div>
-      <h1>All Contacts</h1>
-    </div>
+    <main className="container grid grid-cols-3 gap-10 ">
+      <div className="flex  border-2 mt-10 gap-5 border-red-900 w-[400px] h-[250px]">
+        <div>
+          <img
+            className="w-[200px] object-cover h-[247px]"
+            src="https://c4.wallpaperflare.com/wallpaper/944/995/821/abstract-backgrounds-minimalistic-website-wallpaper-preview.jpg"
+            alt=""
+          />
+        </div>
+        <div className="mt-5">
+          <h1 className="my-3 text-lg font-semibold">Name:</h1>
+          <h1 className="my-3 text-lg font-semibold">Email:</h1>
+          <h1 className="my-3 text-lg font-semibold">Phone Number:</h1>
+          <h1 className="my-3 text-lg font-semibold">Address:</h1>
+        </div>
+      </div>
+      <div className="flex  border-2 mt-10 gap-5 border-red-900 w-[400px] h-[250px]">
+        <div>
+          <img
+            className="w-[200px] object-cover h-[247px]"
+            src="https://c4.wallpaperflare.com/wallpaper/944/995/821/abstract-backgrounds-minimalistic-website-wallpaper-preview.jpg"
+            alt=""
+          />
+        </div>
+        <div className="mt-5">
+          <h1 className="my-3 text-lg font-semibold">Name:</h1>
+          <h1 className="my-3 text-lg font-semibold">Email:</h1>
+          <h1 className="my-3 text-lg font-semibold">Phone Number:</h1>
+          <h1 className="my-3 text-lg font-semibold">Address:</h1>
+        </div>
+      </div>
+      <div className="flex  border-2 mt-10 gap-5 border-red-900 w-[400px] h-[250px]">
+        <div>
+          <img
+            className="w-[200px] object-cover h-[247px]"
+            src="https://c4.wallpaperflare.com/wallpaper/944/995/821/abstract-backgrounds-minimalistic-website-wallpaper-preview.jpg"
+            alt=""
+          />
+        </div>
+        <div className="mt-5">
+          <h1 className="my-3 text-lg font-semibold">Name:</h1>
+          <h1 className="my-3 text-lg font-semibold">Email:</h1>
+          <h1 className="my-3 text-lg font-semibold">Phone Number:</h1>
+          <h1 className="my-3 text-lg font-semibold">Address:</h1>
+        </div>
+      </div>
+      <div className="flex  border-2 mt-10 gap-5 border-red-900 w-[400px] h-[250px]">
+        <div>
+          <img
+            className="w-[200px] object-cover h-[247px]"
+            src="https://c4.wallpaperflare.com/wallpaper/944/995/821/abstract-backgrounds-minimalistic-website-wallpaper-preview.jpg"
+            alt=""
+          />
+        </div>
+        <div className="mt-5">
+          <h1 className="my-3 text-lg font-semibold">Name:</h1>
+          <h1 className="my-3 text-lg font-semibold">Email:</h1>
+          <h1 className="my-3 text-lg font-semibold">Phone Number:</h1>
+          <h1 className="my-3 text-lg font-semibold">Address:</h1>
+        </div>
+      </div>
+    </main>
   );
 };
 
--- src/pages/addContacts/AddContacts.jsx
+++ src/pages/addContacts/AddContacts.jsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import React from "react";
+// import bgImg from "../../assets/addContact/abstract-backgrounds-minimalistic-website-wallpaper-preview.jpg";
 
 import "./AddContact.css";
 
@@ -17,55 +18,74 @@ const AddContacts = () => {
   };
 
   return (
-    <main>
-      <h1 className="flex items-center justify-center mt-10 text-2xl font-semibold">
-        Add Contact
-      </h1>
-      <div className="flex items-center justify-center ">
-        <div class="form-container mt-10 ">
-          <form onSubmit={handleSubmit(handelAddContact)} class="form">
-            <div class="form-group">
-              <label for="name">Name</label>
-              <input
-                {...register("name", { required: true })}
-                name="name"
-                id="name"
-                type="text"
-              />
-              <label for="email"> Email</label>
-              <input
-                {...register("email", { required: true })}
-                name="email"
-                id="email"
-                type="text"
-              />
-              <label for="PhoneNumber"> Phone Number</label>
-              <input
-                {...register("phonNumber", { required: true })}
-                name="phonNumber"
-                id="phonNumber"
-                type="text"
-              />
-              <label for="Address"> Address</label>
-              <input
-                {...register("address", { required: true })}
-                name="address"
-                id="address"
-                type="text"
-              />
-              <label for="Profile picture">picture URL</label>
-              <input
-                {...register("profilePicture", { required: true })}
-                name="profilePicture"
-                id="profilePicture"
-                type="text"
-              />
-            </div>
+    <main className="relative overflow-hidden">
+      <div
+        className="bg-[centertop-1rem]   bg-cover bg-opacity-75 
+        flex items-center justify-center min-h-screen "
+        style={{
+          backgroundImage:
+            'url("https://c4.wallpaperflare.com/wallpaper/944/995/821/abstract-backgrounds-minimalistic-website-wallpaper-preview.jpg")',
+        }}
+      >
+        {/* <img
+          className="flex items-center justify-center w-full h-screen bg-cover "
+          src=''
+          alt=""
+        /> */}
 
-            <button type="submit" class="form-submit-btn">
-              Submit
-            </button>
-          </form>
+        <div className="">
+          <h1 className="flex items-center justify-center mt-10 text-2xl font-semibold text-white">
+            Add Contact
+          </h1>
+          <div className="flex items-center justify-center ">
+            <div className="mt-10 form-container ">
+              <form onSubmit={handleSubmit(handelAddContact)} className=" form">
+                <div className="form-group">
+                  <label className="" for="name">
+                    Name
+                  </label>
+                  <input
+                    {...register("name", { required: true })}
+                    name="name"
+                    id="name"
+                    type="text"
+                  />
+                  <label for="email"> Email</label>
+                  <input
+                    {...register("email", { required: true })}
+                    name="email"
+                    id="email"
+                    type="text"
+                  />
+                  <label for="PhoneNumber"> Phone Number</label>
+                  <input
+                    {...register("phonNumber", { required: true })}
+                    name="phonNumber"
+                    id="phonNumber"
+                    type="text"
+                  />
+                  <label for="Address"> Address</label>
+                  <input
+                    {...register("address", { required: true })}
+                    name="address"
+                    id="address"
+                    type="text"
+                  />
+                  <label for="Profile picture">picture URL</label>
+                  <input
+                    {...register("profilePicture", { required: true })}
+                    name="profilePicture"
+                    id="profilePicture"
+                    type="text"
+                  />
+                </div>
+
+                <button type="submit" class="form-submit-btn">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </main>
