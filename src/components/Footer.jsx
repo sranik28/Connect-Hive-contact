5858dd74048821cb44a643cb4f6d826dbd54afe8
MD Anik <122194751+sranik28@users.noreply.github.com>
2024-01-05 23:54:49 +0600

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,8 +42,6 @@ const Footer = () => {
               </p>
             </div>
           </div>
-
-          
           <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
             <div className="w-full mb-10">
               <h4 className="text-lg font-semibold text-dark mb-9">Pages</h4>
